4bc3e448c2d845680cf52b29334a60ec3b913b90
Caleb Lee <132239576+calebvlee@users.noreply.github.com>
2024-05-24 15:20:03 +1000

    Auto-Internal-Opener-PC.user.js

--- Auto-Internal-Opener-PC.user.js
+++ Auto-Internal-Opener-PC.user.js
@@ -1,19 +1,20 @@
 // ==UserScript==
-// @name        Auto Internal Opener for Athena Chats V1.13 (PC)
-// @namespace   Violentmonkey Scripts
-// @match       https://athena.shopify.io/*
+// @name        Auto Internal Opener for Beacon Athena V1.15 (PC)
+// @namespace   
+// @match       https://beacon.shopify.io/*
 // @grant       none
-// @version     1.13
+// @version     1.15
 // @author      Caleb Lee (Credits to Nicholas Bulmer & Renee Mundie)
-// @description 22/04/2024, 12:04:56 - Use Shift + Control + E
+// @description 22/05/2024 - Use Shift + Control + E
 // ==/UserScript==
 
 
 (function () {
   function openSelectedButtons() {
-    const buttons = document.querySelectorAll(".Polaris-Button_r99lw");
+    // Selecting all button elements since we are not relying on specific class names
+    const buttons = document.querySelectorAll("button");
     console.log('Searching for specified buttons:', buttons);
-    buttons.forEach((button) => {
+    buttons.forEach(button => {
       if (button.innerHTML.includes("Storefront") ||
           button.innerHTML.includes("Admin") ||
           button.innerHTML.includes("Internal")) {
@@ -25,8 +26,8 @@
 
   // Event listener for keydown - trigger on 'Shift' + 'Control' + 'E'
   document.addEventListener("keydown", function (event) {
-      if (event.shiftKey && event.ctrlKey && event.key === 'E') {
-          openSelectedButtons();
-      }
+    if (event.shiftKey && event.ctrlKey && event.key === 'E') {
+      openSelectedButtons();
+    }
   });
 })();
